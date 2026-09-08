3836f22110d7f5e236a4fc598da88cb48e7bb33d
HAIHEX <regongithub572@gmail.com>
2026-09-08 12:20:46 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker terpasang');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Biarkan kosong agar website tetap berjalan normal secara online
-});
